eadfb30ac2e56d1159161f24adc75ba2b0af8966
jneiracampos <j.neira@uniandes.edu.co>
2022-05-15 22:30:15 -0500

    ...

--- CódigoTP3/main.c
+++ CódigoTP3/main.c
@@ -24,11 +24,11 @@ void main()
     printf("\n* - PROGRAMA DE VECTORES - * \n");
     do
     {
-    printf("\nIngrese por favor el tamaño del vector: \n");
+    printf("\nIngrese por favor el tamanio del vector: \n");
     scanf("%d", &tamanioVector); //Se inicializa la variable del tamaño del vector.
     if (tamanioVector <= 0) //Se valida que sea mayor a 0.
     {
-        printf("El valor del tamaño debe ser mayor a 0. \n");
+        printf("El valor del tamanio debe ser mayor a 0. \n");
         continuar = true;
     }else
     {
@@ -40,10 +40,10 @@ void main()
         printf("\nIngrese por favor la cadena de caracteres: \n");
         scanf("%s", vectorCadena);//Se inicializa la cadena
 
-        printf("El vector de int tamaño %d queda representado así: \n", tamanioVector);
+        printf("\nEl vector de int tamanio %d queda representado asi: \n", tamanioVector);
         empaquetamiento(vectorInt, tamanioVector, vectorCadena); //Se invoca el procedimiento que empaqueta la cadena en el vector y se imprime.
     }
-    }while(continuar==true);
+    } while(continuar==true);
 }// main
 
 //Procedimiento que empaqueta la cadena en el vector y la imprime:
@@ -52,17 +52,19 @@ int empaquetamiento(int *vectorInt, int tamanio, char *cadena){
     int lenCadena = strlen(cadena);
 
     //Inicia recorrido de la cadena para convertir de decimal a binario en un byte.
-         /* for (int i = 0; i < lenCadena; i++)
+    /*
+    for (int i = 0; i < lenCadena; i++)
     {
         if (cadena[i] > 64)
         {
             cadena[i] = cadena[i] - 55;
-        }else
+        } else
         {
             cadena[i] = cadena[i] - 48;
         }    
-    }*/
-
+    }
+    */
+    
     __asm{
         mov ebx, cadena // ebx = cadena
         mov eax, 0 //contador
@@ -89,6 +91,7 @@ int empaquetamiento(int *vectorInt, int tamanio, char *cadena){
         fin:
     }
     
+   /*
     for (int i = 0; i < tamanio; i++)
     {
         int caracter = cadena[i*8];
@@ -96,35 +99,37 @@ int empaquetamiento(int *vectorInt, int tamanio, char *cadena){
         {
             caracter = caracter << 4; //Se desplaza a la izquierda ya que los hexa son de 4 bits.
             caracter |= cadena[j]; //Se añade el nuevo hexa.
-            caracter  = cadena[j] | caracter
         }
         vectorInt[i] = caracter;  //Finalmente, se agrega el caracter que se modificó a la posición i del vector.
     }
-
-    int i = 0;
-
-    __asm{
-        firstLoop:
-            move eax, 0 //i
-            imul eax, 8 //i * 8
-            mov ebx, cadena[eax] //Caracter
-            loop:
-                mov ecx, 1
-                add ecx, eax //j
-                shl ebx, 4
-                OR cadena[ecx], ebx
-                inc ecx
+   
+    */
+   int i = 0;
+    
+    __asm {
+        
+        fori:
+            mov ebx, cadena
+            mov eax, i //i
+            imul eax, 8
+            movzx ecx, [ebx+eax] //cl = cadena[i * 8] = caracter
+            forj:
+                shl ecx, 4
                 mov edx, 8
                 add edx, eax
-                cmp ecx, edx //j < 8 + (i * 8)
-                jl loop
-            mov vectorInt[eax], ebx
+                inc eax //j = 1 + (i * 8)
+                cmp eax, edx //j < 8 + (i * 8)
+                jl forj
+            mov ebx, vectorInt
+            mov eax, i
+            mov [ebx+eax], ecx
             inc eax
+            mov i, eax
             mov edx, tamanio
-            cmp eax, edx
-            jl firstLoop
-
+            cmp i, edx //i < tamanio
+            jl fori
     }
+    
 
     //Se recorre el vector para imprimir cada elemento por línea.
     for (int i = 0; i < tamanio; i++)
